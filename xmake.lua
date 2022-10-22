fc520a6c6c5ce6e4055e7058d2f2a21d309c76e2
StarBeat <977663818@qq.com>
2022-10-22 15:31:07 +0800

    xmake add plat

--- xmake.lua
+++ xmake.lua
@@ -3,4 +3,11 @@ add_rules("mode.release")
 target("glfw3")
     set_kind("static")
     add_files("src/*.c")
-    add_defines("WIN32", "_WINDOWS", "NDEBUG", "_GLFW_WIN32", "UNICODE", "_UNICODE")
+    add_cflags("-g", "-DNDEBUG", "-DUNICODE", "-D_UNICODE")
+    if is_plat("windows") then
+        add_defines("_GLFW_WIN32")
+    elseif is_plat("linux") then
+        add_defines("_GLFW_X11")
+    elseif is_plat("macosx") then
+        add_defines("_GLFW_COCOA")
+    end
